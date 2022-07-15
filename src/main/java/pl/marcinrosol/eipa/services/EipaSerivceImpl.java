d6d783db796b25dbf9a5fdeecc90c048658a586c
marcin rosol <marcinrosol1997@gmail.com>
2022-07-15 17:56:28 +0200

    adjustment on api key

--- src/main/java/pl/marcinrosol/eipa/services/EipaSerivceImpl.java
+++ src/main/java/pl/marcinrosol/eipa/services/EipaSerivceImpl.java
@@ -1,5 +1,6 @@
 package pl.marcinrosol.eipa.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.marcinrosol.eipa.models.dao.EipaRequestResult;
@@ -11,10 +12,13 @@ import static pl.marcinrosol.eipa.services.RequestsUrls.DYNAMIC_DATA_URL;
 @Service
 public class EipaSerivceImpl implements EipaService {
 
+    @Value("${api_key}")
+    private String API_KEY;
+
     @Override
     public Optional<EipaRequestResult> fetchDynamicData() {
         RestTemplate restTemplate = new RestTemplate();
-        var result = restTemplate.getForEntity(DYNAMIC_DATA_URL, EipaRequestResult.class);
+        var result = restTemplate.getForEntity(String.format(DYNAMIC_DATA_URL, API_KEY), EipaRequestResult.class);
         return Optional.ofNullable(result.getBody());
     }
 }
